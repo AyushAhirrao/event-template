5551707d2a4798ce092d6ff2cbf5fe3fe016efa9
Ayush Ahirrao <ayushahirrao@gmail.com>
2023-09-13 23:04:13 +0530

    handle cash payment registration code

--- scripts/handle_payment.js
+++ scripts/handle_payment.js
@@ -1,17 +1,37 @@
 let upi_radio_btn = document.getElementById("online");
 let cash_radio_btn = document.getElementById("offline");
 let qr_code = document.getElementById("online-payment")
+let registration_code = document.getElementById("offline-payment")
+const randomCode = generateRandomCode(10);
 
 upi_radio_btn.addEventListener('change', () => {
-    if(upi_radio_btn.checked){
+    if (upi_radio_btn.checked) {
         qr_code.style = "display: flex"
+        registration_code.style = "display: none"
         console.log("display qr")
     }
 })
 
 cash_radio_btn.addEventListener('change', () => {
-    if(cash_radio_btn.checked){
+    if (cash_radio_btn.checked) {
+
         qr_code.style = "display: none"
+        registration_code.style = "display: flex"
+        document.getElementById("registration-id").innerText = `Code - ${randomCode}`
         console.log("hide qr")
     }
 })
+
+
+
+function generateRandomCode(length) {
+    const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let randomCode = "";
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        randomCode += charset[randomIndex];
+    }
+
+    return randomCode;
+}
